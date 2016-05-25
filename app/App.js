b3c5807fb28527a13e56b4f548a1a7d127095f86
Trevor Lambas <tlambas@Coruscant.local>
2016-05-24 20:50:11 -0700

    Adding lots of content

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Header from './components/Header/Header'
+
 class App extends React.Component {
     render() {
-        return
-        (
-            <h1>Hello World</h1>
+        return (
+            <Header />
         )
     }
 }
